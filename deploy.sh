77b0a6e0515e82721bc5171f0c09faba86b2a6e4
Jon Caceres <jcaceres@freeus.com>
2022-10-15 22:33:58 -0400

    Test

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:sarezjay/sarezjay.github.io.git master
+# git push -f git@github.com:sarezjay/sarezjay.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:sarezjay/aptnm.git master:gh-pages
 
 cd -
